8a2d1a8464d3437bb2aa5f09e66b768222b4da86
Anton Fenske <anton@fenske.xyz>
2020-05-20 07:17:15 +0200

    WIP

--- api/accept-results.js
+++ api/accept-results.js
@@ -9,12 +9,16 @@ admin.initializeApp({
 let db = admin.firestore();
 
 export default (req, res) => {
-  let docRef = db.collection('test').doc('test-doc');
+  try {
+    let docRef = db.collection('test').doc('test-doc');
 
-  let setAda = docRef.set({
-    first: 'Ada',
-    last: 'Lovelace',
-    born: 1815
-  });
-  res.json({ result: 'ok' })
+    let setAda = docRef.set({
+      first: 'Ada',
+      last: 'Lovelace',
+      born: 1815
+    });
+    res.json({result: 'ok'})
+  } catch (error) {
+
+  }
 }
