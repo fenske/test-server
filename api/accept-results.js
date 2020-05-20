7ee6700fb3c67dc0fa5ec4496530ab3b6e241427
Anton Fenske <anton@fenske.xyz>
2020-05-20 07:16:22 +0200

    Try integrate firestore

--- api/accept-results.js
+++ api/accept-results.js
@@ -1,3 +1,20 @@
+const admin = require('firebase-admin');
+
+let serviceAccount = require(process.env.GCLOUD_CREDENTIALS);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+let db = admin.firestore();
+
 export default (req, res) => {
-  res.json({ name: 'John', email: 'john@example.com' })
+  let docRef = db.collection('test').doc('test-doc');
+
+  let setAda = docRef.set({
+    first: 'Ada',
+    last: 'Lovelace',
+    born: 1815
+  });
+  res.json({ result: 'ok' })
 }
